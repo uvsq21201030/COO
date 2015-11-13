e3713534295dbd40b37da9ad3095bc856d74d205
uvsq21201030 <alexandre.monteiro@ens.uvsq.fr>
2015-11-13 12:02:56 +0100

    add ex3_3

---
+++ ex3_3/src/main/java/fr/uvsq/coo/fr/uvsq/coo/ex3_3/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.coo.fr.uvsq.coo.ex3_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
