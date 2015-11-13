05b3a9f943a8a0160db0d2bb773c080a19c3a9e8
uvsq21201030 <alexandre.monteiro@ens.uvsq.fr>
2015-11-13 10:38:31 +0100

    “Add

---
+++ TP/src/main/java/fr/uvsq/coo/fr/uvsq/coo/ex3_3/Employe.java
@@ -0,0 +1,16 @@
+package fr.uvsq.coo.fr.uvsq.coo.ex3_3;
+
+class Employe{
+	private final String nom = "";
+	private final String adresse = "";
+	
+	// ...
+	
+	public double calculSalaire(){
+		return /* calcul du salaire*/ ; 
+	}
+	
+	public void afficheCoordonnees(){
+		System.out.println (nom + " , " + adresse); 
+		}
+}
